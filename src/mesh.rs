1ea25fc5f1d7ca2315871a3e737c0a64f261f1cf
Luis Wirth <lwirth2000@gmail.com>
2024-05-30 12:07:04 +0200

    simplicies reference mesh

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,8 +1,162 @@
+use std::rc::Rc;
+
+pub type NodeId = usize;
+
+/// All simplicies of a Mesh are connected to this MeshNodes object.
+/// It contains all the Nodes of the Mesh.
+#[derive(Debug)]
+pub struct MeshNodes(Vec<na::DVector<f64>>);
+impl MeshNodes {
+  pub fn new(nodes: Vec<na::DVector<f64>>) -> Self {
+    Self(nodes)
+  }
+
+  pub fn nodes(&self) -> &[na::DVector<f64>] {
+    &self.0
+  }
+}
+
+/// Not supposed to be mutated, once created.
+#[derive(Debug)]
+pub struct Simplex {
+  /// connection to mesh
+  mesh_nodes: Rc<MeshNodes>,
+  /// sorted nodes of the Simplex
+  nodes: Vec<NodeId>,
+  orientation: Orientation,
+}
+impl PartialEq for Simplex {
+  fn eq(&self, other: &Self) -> bool {
+    self.cmp(other) == Some(Orientation::Pos)
+  }
+}
+
+impl Simplex {
+  pub fn new(mesh_nodes: Rc<MeshNodes>, nodes: Vec<NodeId>) -> Self {
+    Self::new_with_orientation(mesh_nodes, nodes, Default::default())
+  }
+  pub fn new_with_orientation(
+    mesh_nodes: Rc<MeshNodes>,
+    mut nodes: Vec<NodeId>,
+    mut orientation: Orientation,
+  ) -> Self {
+    assert!(!nodes.is_empty(), "Simplex may not be empty.");
+    let swaps = sort_count(&mut nodes);
+    orientation *= Orientation::from(swaps);
+    Self {
+      mesh_nodes,
+      nodes,
+      orientation,
+    }
+  }
+  pub fn dim(&self) -> usize {
+    self.nodes.len() - 1
+  }
+  pub fn vertices(&self) -> &[NodeId] {
+    &self.nodes
+  }
+
+  pub fn cmp(&self, other: &Self) -> Option<Orientation> {
+    if Rc::ptr_eq(&self.mesh_nodes, &other.mesh_nodes) || self.nodes != other.nodes {
+      return None;
+    }
+    Some(match self.orientation == other.orientation {
+      true => Orientation::Pos,
+      false => Orientation::Neg,
+    })
+  }
+
+  /// all direct proper (one dim lower) faces
+  pub fn subs(&self) -> Vec<Simplex> {
+    if self.dim() == 0 {
+      return Vec::new();
+    }
+    (0..self.nodes.len())
+      .map(|i| {
+        let mesh_nodes = Rc::clone(&self.mesh_nodes);
+        let mut nodes = self.nodes.clone();
+        nodes.remove(i);
+        let orientation = Orientation::from(i);
+        Self {
+          mesh_nodes,
+          orientation,
+          nodes,
+        }
+      })
+      .collect()
+  }
+}
+impl std::fmt::Display for Simplex {
+  fn fmt(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+    write!(fmt, "Simplex({})[", self.orientation)?;
+    for v in self.vertices() {
+      write!(fmt, " x_{},", v)?;
+    }
+    write!(fmt, "]")
+  }
+}
+
+/// Not supposed to be mutated, once created.
+#[derive(Debug)]
+pub struct Triangulation {
+  /// Simplicies of highest intrinsic dimension.
+  /// Subsimplicies are not stored, but computed.
+  simplicies: Vec<Simplex>,
+}
+impl Triangulation {
+  pub fn new(simplicies: Vec<Simplex>) -> Self {
+    assert!(!simplicies.is_empty(), "Triangulation may not be empty.");
+    let dim_intrinsic = simplicies[0].dim();
+    assert!(
+      simplicies.iter().all(|s| s.dim() == dim_intrinsic),
+      "Intrinsic dimension of Simplicies in Triangulation must match."
+    );
+
+    Self { simplicies }
+  }
+  pub fn dim_intrinsic(&self) -> usize {
+    self.simplicies[0].dim()
+  }
+  pub fn simplicies(&self) -> &[Simplex] {
+    &self.simplicies
+  }
+}
+
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub enum Orientation {
   #[default]
   Pos,
   Neg,
+}
+impl std::ops::Neg for Orientation {
+  type Output = Self;
+
+  fn neg(self) -> Self::Output {
+    match self {
+      Self::Pos => Self::Neg,
+      Self::Neg => Self::Pos,
+    }
+  }
+}
+impl std::ops::Mul for Orientation {
+  type Output = Self;
+
+  fn mul(self, rhs: Self) -> Self::Output {
+    Self::from(self == rhs)
+  }
+}
+impl std::ops::MulAssign for Orientation {
+  fn mul_assign(&mut self, rhs: Self) {
+    *self = Self::from(*self == rhs);
+  }
+}
+impl From<bool> for Orientation {
+  fn from(b: bool) -> Self {
+    match b {
+      true => Self::Pos,
+      false => Self::Neg,
+    }
+  }
 }
 impl From<usize> for Orientation {
   fn from(n: usize) -> Self {
@@ -26,105 +180,23 @@ impl std::fmt::Display for Orientation {
   }
 }
 
-/// Not supposed to be mutated, once created.
-#[derive(Debug)]
-pub struct Simplex {
-  vertices: Vec<na::DVector<f64>>,
-  orientation: Orientation,
-}
-
-impl Simplex {
-  pub fn new(vertices: Vec<na::DVector<f64>>) -> Self {
-    Self::new_with_orientation(vertices, Default::default())
-  }
-  pub fn new_with_orientation(vertices: Vec<na::DVector<f64>>, orientation: Orientation) -> Self {
-    assert!(!vertices.is_empty(), "Simplex may not be empty.");
-    let dim_ambient = vertices[0].len();
-    assert!(
-      vertices.iter().all(|s| s.len() == dim_ambient),
-      "Vertices in simplex must have same dimension."
-    );
-    Self {
-      vertices,
-      orientation,
-    }
-  }
-  pub fn dim_ambient(&self) -> usize {
-    self.vertices[0].len()
-  }
-  pub fn dim_intrinsic(&self) -> usize {
-    self.vertices.len() - 1
-  }
-  pub fn vertices(&self) -> &[na::DVector<f64>] {
-    &self.vertices
-  }
-
-  /// all direct proper (one dim lower) faces
-  pub fn subs(&self) -> Vec<Simplex> {
-    if self.dim_intrinsic() == 0 {
-      return Vec::new();
-    }
-    (0..self.vertices.len())
-      .map(|i| {
-        let orientation = Orientation::from(i);
-        let mut points = self.vertices.clone();
-        points.remove(i);
-        Simplex {
-          orientation,
-          vertices: points,
-        }
-      })
-      .collect()
-  }
-}
-impl std::fmt::Display for Simplex {
-  fn fmt(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-    write!(fmt, "Simplex({})[\n", self.orientation)?;
-    for (i, v) in self.vertices().iter().enumerate() {
-      write!(fmt, "  x_{}: [", i)?;
-      for c in v {
-        write!(fmt, " {c},")?;
+// TODO: horribly inefficent -> better algorithm
+/// sorts the slice and counts the swaps
+/// bubble sort
+pub fn sort_count<T: Ord>(s: &mut [T]) -> usize {
+  let mut nswaps = 0;
+  let mut swapped = true;
+  while swapped {
+    swapped = false;
+    for i in 1..s.len() {
+      if s[i - 1] > s[i] {
+        s.swap(i - 1, i);
+        nswaps += 1;
+        swapped = true;
       }
-      write!(fmt, "],\n")?;
     }
-    write!(fmt, "]\n")
   }
-}
-
-/// Not supposed to be mutated, once created.
-#[derive(Debug)]
-pub struct Triangulation {
-  /// Simplicies of highest intrinsic dimension.
-  /// Subsimplicies are not stored, but computed.
-  simplicies: Vec<Simplex>,
-}
-impl Triangulation {
-  pub fn new(simplicies: Vec<Simplex>) -> Self {
-    assert!(!simplicies.is_empty(), "Triangulation may not be empty.");
-    let dim_ambient = simplicies[0].dim_ambient();
-    assert!(
-      simplicies.iter().all(|s| s.dim_ambient() == dim_ambient),
-      "Ambient dimension of Simplicies in Triangulation must match."
-    );
-    let dim_intrinsic = simplicies[0].dim_intrinsic();
-    assert!(
-      simplicies
-        .iter()
-        .all(|s| s.dim_intrinsic() == dim_intrinsic),
-      "Intrinsic dimension of Simplicies in Triangulation must match."
-    );
-
-    Self { simplicies }
-  }
-  pub fn dim_ambient(&self) -> usize {
-    self.simplicies[0].dim_ambient()
-  }
-  pub fn dim_intrinsic(&self) -> usize {
-    self.simplicies[0].dim_intrinsic()
-  }
-  pub fn simplicies(&self) -> &[Simplex] {
-    &self.simplicies
-  }
+  nswaps
 }
 
 #[cfg(test)]
@@ -133,22 +205,18 @@ mod tests {
 
   #[test]
   fn unit_squared_2d_triangulation() {
-    let unit_triangle = Simplex::new(vec![
+    let nodes = MeshNodes::new(vec![
       na::DVector::from_column_slice(&[0.0, 0.0]),
       na::DVector::from_column_slice(&[1.0, 0.0]),
-      na::DVector::from_column_slice(&[0.0, 1.0]),
-    ]);
-    assert_eq!(unit_triangle.dim_ambient(), 2);
-    assert_eq!(unit_triangle.dim_intrinsic(), 2);
-    let other_triangle = Simplex::new(vec![
       na::DVector::from_column_slice(&[1.0, 1.0]),
       na::DVector::from_column_slice(&[0.0, 1.0]),
-      na::DVector::from_column_slice(&[1.0, 0.0]),
     ]);
-    assert_eq!(other_triangle.dim_ambient(), 2);
-    assert_eq!(other_triangle.dim_intrinsic(), 2);
+    let nodes = Rc::new(nodes);
+    let unit_triangle = Simplex::new(nodes.clone(), vec![0, 1, 3]);
+    assert_eq!(unit_triangle.dim(), 2);
+    let other_triangle = Simplex::new(nodes, vec![1, 2, 3]);
+    assert_eq!(other_triangle.dim(), 2);
     let triangulation = Triangulation::new(vec![unit_triangle, other_triangle]);
-    assert_eq!(triangulation.dim_ambient(), 2);
     assert_eq!(triangulation.dim_intrinsic(), 2);
     let edges: Vec<_> = triangulation
       .simplicies()
