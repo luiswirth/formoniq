37b8a33103af866d1247890dcd5e2fa0ae52f053
Luis Wirth <lwirth2000@gmail.com>
2024-10-25 18:30:17 +0200

    gmsh cell loading

--- examples/poisson.rs
+++ examples/poisson.rs
@@ -1,16 +1,14 @@
 //! Solves a manufactored poisson problem in d dimensions
 //! and determines the algebraic convergence rate.
 
+#[path = "common.rs"]
+mod common;
+
 extern crate nalgebra as na;
 extern crate nalgebra_sparse as nas;
 
-use formoniq::{
-  assemble::{self, assemble_galmat, assemble_galvec},
-  fe::{l2_norm, laplacian_neg_elmat, LoadElvec},
-  matrix::FaerCholesky,
-  mesh::{coordinates::MeshNodeCoords, hyperbox::HyperBoxMeshInfo, util::NodeData},
-  space::FeSpace,
-};
+use common::solve_manufactured_poisson;
+use formoniq::{fe::l2_norm, mesh::hyperbox::HyperBoxMeshInfo, space::FeSpace};
 
 use std::rc::Rc;
 
@@ -74,7 +72,7 @@ fn main() {
     let space = FeSpace::new(mesh.clone());
 
     // Compute Galerkin solution to manufactored poisson problem.
-    let galsol = solve_poisson(&nodes, &space, analytic_sol, analytic_laplacian);
+    let galsol = solve_manufactured_poisson(&nodes, &space, analytic_sol, analytic_laplacian);
 
     // Compute analytical solution on mesh nodes.
     let analytical_sol = na::DVector::from_iterator(
@@ -98,51 +96,4 @@ fn main() {
     );
   }
   print_seperator();
-}
-
-fn solve_poisson<F, G>(
-  nodes: &MeshNodeCoords,
-  space: &FeSpace,
-  analytic_sol: F,
-  analytic_laplacian: G,
-) -> na::DVector<f64>
-where
-  F: Fn(na::DVectorView<f64>) -> f64,
-  G: Fn(na::DVectorView<f64>) -> f64,
-{
-  let d = space.mesh().dim();
-
-  // Assemble galerkin matrix and galerkin vector.
-  let mut galmat = assemble_galmat(space, laplacian_neg_elmat);
-  let mut galvec = assemble_galvec(
-    space,
-    LoadElvec::new(NodeData::from_coords_map(nodes, |x| -analytic_laplacian(x))),
-  );
-
-  // Enforce homogeneous Dirichlet boundary conditions
-  // by fixing dofs on boundary.
-  let nodes_per_dim = (nodes.nnodes() as f64).powf((d as f64).recip()) as usize;
-  assemble::fix_dof_coeffs(
-    |mut idof| {
-      let mut fcoord = na::DVector::zeros(d);
-      let mut is_boundary = false;
-      for dim in 0..d {
-        let icoord = idof % nodes_per_dim;
-        fcoord[dim] = icoord as f64 / (nodes_per_dim - 1) as f64;
-        is_boundary |= icoord == 0 || icoord == nodes_per_dim - 1;
-        idof /= nodes_per_dim;
-      }
-
-      is_boundary.then_some(analytic_sol(fcoord.as_view()))
-    },
-    &mut galmat,
-    &mut galvec,
-  );
-
-  let galmat = galmat.to_nalgebra();
-
-  // Obtain Galerkin solution by solving LSE.
-  let galsol = FaerCholesky::new(galmat).solve(&galvec).column(0).into();
-
-  galsol
 }
---
+++ src/mesh/gmsh.rs
@@ -0,0 +1,54 @@
+use crate::mesh::{
+  coordinates::{CoordManifold, MeshNodeCoords},
+  raw::RawSimplexTopology,
+};
+use tracing::warn;
+
+/// Load Gmesh `.msh` file (version 4.1).
+pub fn gmsh2coord_mesh(bytes: &[u8]) -> CoordManifold {
+  let msh = mshio::parse_msh_bytes(bytes).unwrap();
+
+  let mesh_nodes = msh.data.nodes.unwrap().node_blocks;
+  let mesh_nodes: Vec<_> = mesh_nodes
+    .iter()
+    .flat_map(|block| block.nodes.iter())
+    .map(|node| na::DVector::from_column_slice(&[node.x, node.y, node.z]))
+    .collect();
+  let mesh_nodes = na::DMatrix::from_columns(&mesh_nodes);
+  let mesh_nodes = MeshNodeCoords::new(mesh_nodes);
+
+  let mut points = Vec::new();
+  let mut edges = Vec::new();
+  let mut trias = Vec::new();
+  let mut quads = Vec::new();
+
+  let elements = msh.data.elements.unwrap();
+  for block in elements.element_blocks {
+    type ElType = mshio::ElementType;
+    let simplex_acc = match block.element_type {
+      ElType::Pnt => &mut points,
+      ElType::Lin2 => &mut edges,
+      ElType::Tri3 => &mut trias,
+      ElType::Tet4 => &mut quads,
+      _ => {
+        warn!("unsupported gmsh ElementType: {:?}", block.element_type);
+        continue;
+      }
+    };
+    for e in block.elements {
+      let simplex = e.nodes.iter().map(|tag| *tag as usize - 1).collect();
+      simplex_acc.push(RawSimplexTopology::new(simplex));
+    }
+  }
+
+  if !quads.is_empty() {
+    return CoordManifold::new(quads, mesh_nodes);
+  }
+  if !trias.is_empty() {
+    return CoordManifold::new(trias, mesh_nodes);
+  }
+  if !edges.is_empty() {
+    return CoordManifold::new(edges, mesh_nodes);
+  }
+  panic!("failed to construct Triangulation from gmsh");
+}
