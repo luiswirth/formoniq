c4606df9bc840a8d08b754bff012bbc9f421b446
Luis Wirth <lwirth2000@gmail.com>
2024-12-13 16:30:10 +0100

    curvature flow: support mesh with boundary

--- examples/curvature_flow.rs
+++ examples/curvature_flow.rs
@@ -5,7 +5,7 @@ extern crate nalgebra_sparse as nas;
 
 use formoniq::{
   assemble, fe,
-  mesh::gen::dim3::{self, TriangleSurface3D},
+  mesh::{coordinates::VertexCoords, gen::dim3},
   util::FaerCholesky,
   Dim,
 };
@@ -18,45 +18,104 @@ fn main() {
 
   const EXTRINSIC_DIM: Dim = 3;
 
-  let mesh_subdivisions = 5;
-  let surface = dim3::mesh_sphere_surface(mesh_subdivisions);
-  std::fs::write("out/curvature_flow0.obj", surface.to_obj_string()).unwrap();
+  let obj_path = std::env::var("FORMONIQ_OBJ_PATH")
+    .expect("specify the OBJ file using the envvar `FORMONIQ_OBJ_PATH`");
+  let obj_string = std::fs::read_to_string(obj_path).unwrap();
+  let surface = dim3::TriangleSurface3D::from_obj_string(&obj_string);
+  std::fs::write("out/curvature_flow.obj", surface.to_obj_string()).unwrap();
 
-  let mut coord_mesh = surface.into_coord_manifold();
+  let coord_mesh = surface.into_coord_manifold();
   let mut mesh = coord_mesh.to_riemannian_complex();
 
+  let mut coords_list = vec![coord_mesh.coords().clone()];
+
   let dt = 1e-3;
-  let nsteps = 30;
+  let nsteps = 10;
 
-  let last_step = nsteps - 1;
-  for istep in 0..nsteps {
-    println!("Solving Curvature Flow at step={istep}/{last_step}...");
+  if mesh.has_boundary() {
+    let last_step = nsteps - 1;
+    for istep in 0..nsteps {
+      println!("Solving Curvature Flow at step={istep}/{last_step}...");
 
-    let laplace = assemble::assemble_galmat(&mesh, fe::laplace_beltrami_elmat);
-    let mass = assemble::assemble_galmat(&mesh, fe::mass_elmat);
-    let source = na::DVector::zeros(mesh.nvertices());
+      let laplace = assemble::assemble_galmat(&mesh, fe::laplace_beltrami_elmat);
+      let mass = assemble::assemble_galmat(&mesh, fe::mass_elmat);
+      let source = na::DVector::zeros(mesh.nvertices());
 
-    let laplace = laplace.to_nalgebra_csc();
-    let mass = mass.to_nalgebra_csc();
+      let coords_initial = coords_list.first().unwrap().matrix();
+      let coords_old = coords_list.last().unwrap().matrix();
+      let mut coords_new = na::DMatrix::zeros(EXTRINSIC_DIM, mesh.nvertices());
+      for d in 0..EXTRINSIC_DIM {
+        let comps_initial = coords_initial.row(d).transpose();
+        let comps_old = coords_old.row(d).transpose();
 
-    let lse_matrix = &mass + dt * &laplace;
-    let lse_cholesky = FaerCholesky::new(lse_matrix);
+        let mut laplace = laplace.clone();
+        let mut mass = mass.clone();
+        let mut source = source.clone();
 
-    let mut new_coords = coord_mesh.coords().clone();
-    for d in 0..EXTRINSIC_DIM {
-      let components = coord_mesh.coords().matrix().row(d).transpose();
-      let rhs = &mass * components + dt * &source;
-      let new_components = lse_cholesky.solve(&rhs);
-      new_coords
-        .matrix_mut()
-        .row_mut(d)
-        .copy_from(&new_components.transpose());
+        let boundary_data = |inode| comps_initial[inode];
+        assemble::enforce_dirichlet_bc(&mesh, boundary_data, &mut laplace, &mut source);
+        assemble::enforce_dirichlet_bc(&mesh, boundary_data, &mut mass, &mut source);
+
+        let laplace = laplace.to_nalgebra_csc();
+        let mass = mass.to_nalgebra_csc();
+
+        let lse_matrix = &mass + dt * &laplace;
+        let lse_cholesky = FaerCholesky::new(lse_matrix);
+
+        let rhs = &mass * comps_old + dt * &source;
+        let comps_new = lse_cholesky.solve(&rhs).transpose();
+        coords_new.row_mut(d).copy_from(&comps_new);
+      }
+
+      let coords_new = VertexCoords::new(coords_new);
+      *mesh.edge_lengths_mut() = coords_new.to_edge_lengths(mesh.complex());
+      coords_list.push(coords_new);
     }
+  } else {
+    let last_step = nsteps - 1;
+    for istep in 0..nsteps {
+      println!("Solving Curvature Flow at step={istep}/{last_step}...");
 
-    *coord_mesh.coords_mut() = new_coords;
-    *mesh.edge_lengths_mut() = coord_mesh.coords().to_edge_lengths(mesh.complex());
+      let laplace = assemble::assemble_galmat(&mesh, fe::laplace_beltrami_elmat);
+      let mass = assemble::assemble_galmat(&mesh, fe::mass_elmat);
+      let source = na::DVector::zeros(mesh.nvertices());
+
+      let laplace = laplace.to_nalgebra_csc();
+      let mass = mass.to_nalgebra_csc();
+
+      let lse_matrix = &mass + dt * &laplace;
+      let lse_cholesky = FaerCholesky::new(lse_matrix);
+
+      let coords_old = coords_list.last().unwrap().matrix();
+      let mut coords_new = na::DMatrix::zeros(EXTRINSIC_DIM, mesh.nvertices());
+      for d in 0..EXTRINSIC_DIM {
+        let comps_old = coords_old.row(d).transpose();
+
+        let rhs = &mass * comps_old + dt * &source;
+        let comps_new = lse_cholesky.solve(&rhs).transpose();
+        coords_new.row_mut(d).copy_from(&comps_new);
+      }
+
+      let coords_new = VertexCoords::new(coords_new);
+      *mesh.edge_lengths_mut() = coords_new.to_edge_lengths(mesh.complex());
+      coords_list.push(coords_new);
+    }
   }
 
-  let surface = TriangleSurface3D::from_coord_manifold(coord_mesh);
-  std::fs::write("out/curvature_flow1.obj", surface.to_obj_string()).unwrap();
+  let mdd_frames: Vec<Vec<[f32; 3]>> = coords_list
+    .into_iter()
+    .map(|coords| {
+      let frame_coords: Vec<[f32; 3]> = coords
+        .matrix()
+        .column_iter()
+        .map(|col| [col[0] as f32, col[1] as f32, col[2] as f32])
+        .collect();
+
+      frame_coords
+    })
+    .collect();
+  let times: Vec<_> = (0..=nsteps).map(|istep| dt as f32 * istep as f32).collect();
+
+  println!("Writing animation to `.mdd` file...");
+  dim3::write_mdd_file("out/curvature_flow.mdd", &mdd_frames, &times).unwrap();
 }
--- src/sparse.rs
+++ src/sparse.rs
@@ -1,4 +1,4 @@
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct SparseMatrix {
   nrows: usize,
   ncols: usize,
