9da8a3658fb9e75abd268c1274fc176c4752d16a
Luis Wirth <lwirth2000@gmail.com>
2025-01-16 17:34:03 +0100

    orthonormalization of metric

--- crates/geometry/src/metric.rs
+++ crates/geometry/src/metric.rs
@@ -1,10 +1,125 @@
 pub mod manifold;
 
+use common::linalg::DMatrixExt;
 use index_algebra::IndexSet;
 use itertools::Itertools;
 use topology::Dim;
 
 pub type EdgeIdx = usize;
+
+#[derive(Debug, Clone)]
+pub struct RiemannianMetric {
+  metric_tensor: na::DMatrix<f64>,
+  inverse_metric_tensor: na::DMatrix<f64>,
+}
+impl RiemannianMetric {
+  pub fn new(metric_tensor: na::DMatrix<f64>) -> Self {
+    let n = metric_tensor.nrows();
+    // WARN: Numerically Unstable. TODO: can we avoid this?
+    let inverse_metric_tensor = metric_tensor
+      .clone()
+      .cholesky()
+      .unwrap()
+      .solve(&na::DMatrix::identity(n, n));
+    Self {
+      metric_tensor,
+      inverse_metric_tensor,
+    }
+  }
+
+  pub fn from_tangent_vectors(tangent_vectors: na::DMatrix<f64>) -> Self {
+    let metric_tensor = tangent_vectors.gramian();
+    Self::new(metric_tensor)
+  }
+
+  /// Orthonormal metric.
+  pub fn euclidean(dim: Dim) -> Self {
+    let identity = na::DMatrix::identity(dim, dim);
+    let metric_tensor = identity.clone();
+    let inverse_metric_tensor = identity;
+    Self {
+      metric_tensor,
+      inverse_metric_tensor,
+    }
+  }
+
+  pub fn metric_tensor(&self) -> &na::DMatrix<f64> {
+    &self.metric_tensor
+  }
+  pub fn inverse_metric_tensor(&self) -> &na::DMatrix<f64> {
+    &self.inverse_metric_tensor
+  }
+
+  pub fn inner(&self, i: usize, j: usize) -> f64 {
+    self.metric_tensor[(i, j)]
+  }
+  pub fn length_sqr(&self, i: usize) -> f64 {
+    self.inner(i, i)
+  }
+  pub fn length(&self, i: usize) -> f64 {
+    self.length_sqr(i).sqrt()
+  }
+  pub fn angle_cos(&self, i: usize, j: usize) -> f64 {
+    self.inner(i, j) / self.length(i) / self.length(j)
+  }
+  pub fn angle(&self, i: usize, j: usize) -> f64 {
+    self.angle_cos(i, j).acos()
+  }
+
+  pub fn dim(&self) -> Dim {
+    self.metric_tensor.nrows()
+  }
+  pub fn det(&self) -> f64 {
+    self.metric_tensor.determinant()
+  }
+  pub fn det_sqrt(&self) -> f64 {
+    self.det().sqrt()
+  }
+
+  /// Orthonormal (w.r.t. metric) vectors expressed using old basis.
+  ///
+  /// Orthonormalizes metric tensor $I = B^T G B = B^T V^T V B = (V B)^T (V B)$
+  pub fn orthonormal_basis(&self) -> na::DMatrix<f64> {
+    let na::SymmetricEigen {
+      eigenvalues,
+      mut eigenvectors,
+    } = self.metric_tensor.clone().symmetric_eigen();
+    for (eigenvalue, mut eigenvector) in eigenvalues.iter().zip(eigenvectors.column_iter_mut()) {
+      eigenvector /= eigenvalue.sqrt();
+    }
+    eigenvectors
+  }
+
+  /// Gram matrix on tangent vector standard basis.
+  pub fn vector_gramian(&self) -> &na::DMatrix<f64> {
+    &self.metric_tensor
+  }
+  pub fn vector_inner_product(
+    &self,
+    v: &na::DMatrix<f64>,
+    w: &na::DMatrix<f64>,
+  ) -> na::DMatrix<f64> {
+    v.transpose() * self.vector_gramian() * w
+  }
+  pub fn vector_norm_sqr(&self, v: &na::DMatrix<f64>) -> na::DMatrix<f64> {
+    self.vector_inner_product(v, v)
+  }
+
+  /// Gram matrix on tangent covector standard basis.
+  pub fn covector_gramian(&self) -> &na::DMatrix<f64> {
+    &self.inverse_metric_tensor
+  }
+  pub fn covector_inner_product(
+    &self,
+    v: &na::DMatrix<f64>,
+    w: &na::DMatrix<f64>,
+  ) -> na::DMatrix<f64> {
+    v.transpose() * self.covector_gramian() * w
+  }
+  pub fn covector_norm_sqr(&self, v: &na::DMatrix<f64>) -> na::DMatrix<f64> {
+    self.covector_inner_product(v, v)
+  }
+}
 
 /// Global or local list of edge lengths.
 #[derive(Debug, Clone)]
@@ -85,76 +200,28 @@ impl EdgeLengths {
   }
 }
 
-#[derive(Debug, Clone)]
-pub struct RiemannianMetric {
-  metric_tensor: na::DMatrix<f64>,
-  inverse_metric_tensor: na::DMatrix<f64>,
-}
-impl RiemannianMetric {
-  pub fn new(metric_tensor: na::DMatrix<f64>) -> Self {
-    // WARN: Numerically Unstable. TODO: can we avoid this?
-    let inverse_metric_tensor = metric_tensor.clone().try_inverse().unwrap();
-    Self {
-      metric_tensor,
-      inverse_metric_tensor,
-    }
-  }
+#[cfg(test)]
+mod test {
+  use super::*;
 
-  pub fn euclidean(dim: Dim) -> Self {
-    let identity = na::DMatrix::identity(dim, dim);
-    let metric_tensor = identity.clone();
-    let inverse_metric_tensor = identity;
-    Self {
-      metric_tensor,
-      inverse_metric_tensor,
-    }
-  }
+  #[test]
+  fn orthonormal_basis() {
+    let dim = 2;
+    let tangent_vectors = na::dmatrix![
+      1.0, 0.0;
+      1.0, 1.0;
+    ];
+    let metric = RiemannianMetric::from_tangent_vectors(tangent_vectors.clone());
+    let orthonormal_basis = metric.orthonormal_basis();
+    assert!(
+      (orthonormal_basis.transpose() * metric.metric_tensor * &orthonormal_basis
+        - na::DMatrix::identity(dim, dim))
+      .norm()
+        <= 1e-12
+    );
 
-  pub fn metric_tensor(&self) -> &na::DMatrix<f64> {
-    &self.metric_tensor
-  }
-  pub fn inverse_metric_tensor(&self) -> &na::DMatrix<f64> {
-    &self.inverse_metric_tensor
-  }
-
-  pub fn dim(&self) -> Dim {
-    self.metric_tensor.nrows()
-  }
-
-  pub fn det(&self) -> f64 {
-    self.metric_tensor.determinant()
-  }
-  pub fn det_sqrt(&self) -> f64 {
-    self.det().sqrt()
-  }
-
-  /// Gram matrix on tangent vector standard basis.
-  pub fn vector_gramian(&self) -> &na::DMatrix<f64> {
-    &self.metric_tensor
-  }
-  pub fn vector_inner_product(
-    &self,
-    v: &na::DMatrix<f64>,
-    w: &na::DMatrix<f64>,
-  ) -> na::DMatrix<f64> {
-    v.transpose() * self.vector_gramian() * w
-  }
-  pub fn vector_norm_sqr(&self, v: &na::DMatrix<f64>) -> na::DMatrix<f64> {
-    self.vector_inner_product(v, v)
-  }
-
-  /// Gram matrix on tangent covector standard basis.
-  pub fn covector_gramian(&self) -> &na::DMatrix<f64> {
-    &self.inverse_metric_tensor
-  }
-  pub fn covector_inner_product(
-    &self,
-    v: &na::DMatrix<f64>,
-    w: &na::DMatrix<f64>,
-  ) -> na::DMatrix<f64> {
-    v.transpose() * self.covector_gramian() * w
-  }
-  pub fn covector_norm_sqr(&self, v: &na::DMatrix<f64>) -> na::DMatrix<f64> {
-    self.covector_inner_product(v, v)
+    let orthonormal_tangent_vectors = tangent_vectors * orthonormal_basis;
+    let orthogonal_metric = RiemannianMetric::from_tangent_vectors(orthonormal_tangent_vectors);
+    assert!((orthogonal_metric.metric_tensor() - na::DMatrix::identity(dim, dim)).norm() <= 1e-12);
   }
 }
