a15364c94ae11254b225099127c60b5d008f75b1
Luis Wirth <lwirth2000@gmail.com>
2025-04-26 17:20:17 +0200

    simplify poisson example

--- crates/formoniq/examples/poisson.rs
+++ crates/formoniq/examples/poisson.rs
@@ -3,16 +3,13 @@
 
 use {
   common::util::algebraic_convergence_rate,
-  ddf::cochain::{cochain_projection, Cochain},
+  ddf::cochain::cochain_projection,
   exterior::field::DiffFormClosure,
   formoniq::{
     fe::{hdif_norm, l2_norm},
     problems::laplace_beltrami,
   },
-  manifold::{
-    gen::cartesian::CartesianMeshInfo, geometry::metric::mesh::MeshLengths,
-    topology::complex::Complex,
-  },
+  manifold::gen::cartesian::CartesianMeshInfo,
 };
 
 use std::f64::consts::TAU;
@@ -28,85 +25,61 @@ fn main() {
     let laplacian = DiffFormClosure::scalar(|x| x.iter().map(|x| x.sin()).sum(), dim);
 
     let nrefinements = 20 / dim;
-    let setups = (0..nrefinements)
-      .map(move |refinement| {
-        let nboxes_per_dim = 2usize.pow(refinement as u32);
+    let mut errors_l2 = Vec::with_capacity(nrefinements);
+    let mut errors_h1 = Vec::with_capacity(nrefinements);
 
-        // Mesh of hypercube $[0, tau]^d$.
-        let box_mesh = CartesianMeshInfo::new_unit_scaled(dim, nboxes_per_dim, TAU);
-        let (topology, coords) = box_mesh.compute_coord_complex();
+    fn print_seperator() {
+      let nchar = 24;
+      println!("{}", "-".repeat(nchar));
+    }
 
-        let solution_exact = cochain_projection(&solution_exact, &topology, &coords, None);
-        let laplacian = cochain_projection(&laplacian, &topology, &coords, None);
+    print_seperator();
+    println!("| {:>2} | {:>6} | {:>6} |", "k", "L2", "H1",);
+    print_seperator();
 
-        let metric = coords.to_edge_lengths(&topology);
+    for refinement in 0..nrefinements {
+      let nboxes_per_dim = 2usize.pow(refinement as u32);
 
-        PoissonWithSol {
-          topology,
-          metric,
-          solution_exact,
-          load_data: laplacian,
-        }
-      })
-      .collect();
+      // Mesh of hypercube $[0, tau]^d$.
+      let box_mesh = CartesianMeshInfo::new_unit_scaled(dim, nboxes_per_dim, TAU);
+      let (topology, coords) = box_mesh.compute_coord_complex();
 
-    measure_convergence(setups);
+      let laplacian = cochain_projection(&laplacian, &topology, &coords, None);
+      let solution_exact = cochain_projection(&solution_exact, &topology, &coords, None);
+
+      let metric = coords.to_edge_lengths(&topology);
+
+      let boundary_data = |ivertex| solution_exact[ivertex];
+      let galsol = laplace_beltrami::solve_laplace_beltrami_source(
+        &topology,
+        &metric,
+        laplacian,
+        boundary_data,
+      );
+
+      let difference = solution_exact - galsol;
+
+      let conv_rate = |errors: &[f64], curr: f64| {
+        errors
+          .last()
+          .map(|&prev| algebraic_convergence_rate(curr, prev))
+          .unwrap_or(f64::INFINITY)
+      };
+
+      let error_l2 = l2_norm(&difference, &topology, &metric);
+      let conv_rate_l2 = conv_rate(&errors_l2, error_l2);
+      errors_l2.push(error_l2);
+
+      let error_h1 = hdif_norm(&difference, &topology, &metric);
+      let conv_rate_h1 = conv_rate(&errors_h1, error_h1);
+      errors_h1.push(error_h1);
+
+      println!(
+        "| {:>2} | {:>6.2} | {:>6.2} |",
+        refinement, conv_rate_l2, conv_rate_h1,
+      );
+    }
+
+    print_seperator();
   }
-}
-
-struct PoissonWithSol {
-  topology: Complex,
-  metric: MeshLengths,
-  load_data: Cochain,
-  solution_exact: Cochain,
-}
-
-/// Supply analytic solution and analytic (negative) Laplacian
-fn measure_convergence(refined_setups: Vec<PoissonWithSol>) {
-  fn print_seperator() {
-    let nchar = 24;
-    println!("{}", "-".repeat(nchar));
-  }
-
-  print_seperator();
-  println!("| {:>2} | {:>6} | {:>6} |", "k", "L2", "H1",);
-  print_seperator();
-
-  let mut errors_l2 = Vec::with_capacity(refined_setups.len());
-  let mut errors_h1 = Vec::with_capacity(refined_setups.len());
-  for (refinement_level, setup) in refined_setups.into_iter().enumerate() {
-    let PoissonWithSol {
-      topology,
-      metric,
-      load_data,
-      solution_exact,
-    } = setup;
-
-    let boundary_data = |ivertex| solution_exact[ivertex];
-    let galsol =
-      laplace_beltrami::solve_laplace_beltrami_source(&topology, &metric, load_data, boundary_data);
-
-    let difference = solution_exact - galsol;
-
-    let conv_rate = |errors: &[f64], curr: f64| {
-      errors
-        .last()
-        .map(|&prev| algebraic_convergence_rate(curr, prev))
-        .unwrap_or(f64::INFINITY)
-    };
-
-    let error_l2 = l2_norm(&difference, &topology, &metric);
-    let conv_rate_l2 = conv_rate(&errors_l2, error_l2);
-    errors_l2.push(error_l2);
-
-    let error_h1 = hdif_norm(&difference, &topology, &metric);
-    let conv_rate_h1 = conv_rate(&errors_h1, error_h1);
-    errors_h1.push(error_h1);
-
-    println!(
-      "| {:>2} | {:>6.2} | {:>6.2} |",
-      refinement_level, conv_rate_l2, conv_rate_h1,
-    );
-  }
-  print_seperator();
 }
